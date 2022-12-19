cd487f7b1826ed59fed7ff4fde000123dc6ef39c
liu2830 <2305195328@qq.com>
2022-12-19 14:25:51 +0800

    new file:   .vscode/launch.json 	new file:   .vscode/settings.json 	new file:   .vscode/tasks.json 	new file:   Makefile 	new file:   include/leap_year/leap_year.c 	new file:   include/leap_year/leap_year.h 	new file:   output/main.exe 	new file:   src/main.c 	new file:   src/main.o

---
+++ src/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "./leap_year/leap_year.h"
+
+int main(int argc, char **argv)
+{
+	//? 调试用
+	// printf("argc:%d\n", argc);
+	// printf("argv0:%s\n", argv[0]);
+	// printf("argv1:%s\n", argv[1]);
+	// // argv[1] = 1952;
+
+	//? 测试func_LeapYear函数
+	// for (int i = 1950; i < 2050; i++)
+	// {
+	// 	if(func_LeapYear(i))
+	// 	// if (func_LeapYear(atoi(argv[1])))
+	// 		printf("%d ",i);
+	// 	// else
+	// 	// 	printf("flase");
+	// }
+
+	return (0);
+
+}
