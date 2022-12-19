b269a7de0eae6c850fad94b48e5d7441d563fd9c
liu2830 <2305195328@qq.com>
2022-12-19 20:45:29 +0800

    new file:   include/algorithm_MyWeek/algorithm_MyWeek.c 	new file:   include/algorithm_MyWeek/algorithm_MyWeek.h 	modified:   output/main.exe 	modified:   src/main.c 	modified:   src/main.o

--- src/main.c
+++ src/main.c
@@ -1,15 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "./leap_year/leap_year.h"
+
+/********************************************************************************
+ * @brief 判断输入的字符串是否有误，输入正确则返回转换后的整数类型
+ * 
+ * @param  _str             
+ * @return int              
+ ********************************************************************************/
+int argv2(char **_str)
+{
+
+	if (!atoi(_str))
+	{
+		printf("输入有误,程序退出\n");
+		printf("Input error\n");
+		exit(1);
+	}
+	return atoi(_str); // atoi函数为字符串转换整数类型
+}
 
 int main(int argc, char **argv)
 {
 	//? 调试用
-	// printf("argc:%d\n", argc);
-	// printf("argv0:%s\n", argv[0]);
-	// printf("argv1:%s\n", argv[1]);
-	// // argv[1] = 1952;
+	printf("argc:%d\n", argc);
+	printf("argv0:%s\n", argv[0]);
+	printf("argv1:%s\n", argv[1]);
+	// argv[1] = 1952;
 
-	//? 测试func_LeapYear函数
+	// //? 测试func_LeapYear函数
 	// for (int i = 1950; i < 2050; i++)
 	// {
 	// 	if(func_LeapYear(i))
@@ -18,7 +37,25 @@ int main(int argc, char **argv)
 	// 	// else
 	// 	// 	printf("flase");
 	// }
+	
+	if(argc != 2)
+	{
+		char str[4];
+		printf("可接收程序参数(保留4位)main.exe <年份>\n例如main.exe 2022\n");
+		printf("\nmethod of application\tmain.exe {year}\nsuh as\tmain.exe 2022\n");
+		printf("输入一个值(input):");
+		scanf("%s",str);
+		str[4] = '\0'; // 防止溢出异常
+		int year = argv2(str);
+		printf("输入年份为(Your input is):%d\n", year);
+	}
+	else
+	{
+		argv[1][4] = '\0'; // 防止溢出异常
+		int year = argv2(argv[1]);
+		printf("输入年份为(Your input is): % d\n ", year);
+	}
+
 	printf("--test--\n");
 	return (0);
-
 }
