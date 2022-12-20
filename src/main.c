e4c463beb057e2fcc0039969d77943ff825c79bb
liu2830 <2305195328@qq.com>
2022-12-20 17:51:41 +0800

    modified:   .vscode/launch.json 	modified:   .vscode/settings.json 	new file:   include/show_calendar/show_calendar.c 	new file:   include/show_calendar/show_calendar.h 	modified:   output/main.exe 	modified:   src/main.c 	modified:   src/main.o

--- src/main.c
+++ src/main.c
@@ -16,6 +16,7 @@ int argv2(char *_str)
 	{
 		printf("输入有误,程序退出\n");
 		printf("Input error\n");
+		system("pause");
 		exit(1);
 	}
 	return atoi(_str); // atoi函数为字符串转换整数类型
@@ -23,37 +24,41 @@ int argv2(char *_str)
 
 int main(int argc, char **argv)
 {
-	//? 调试用
-	printf("argc:%d\n", argc);
-	printf("argv0:%s\n", argv[0]);
-	printf("argv1:%s\n", argv[1]);
-	// argv[1] = 1952;
 
-	// //? 测试func_LeapYear函数
-	// for (int i = 1950; i < 2050; i++)
-	// {
-	// 	if(func_LeapYear(i))
-	// 	// if (func_LeapYear(atoi(argv[1])))
-	// 		printf("%d ",i);
-	// 	// else
-	// 	// 	printf("flase");
-	// }
+	//设置中文不乱码
+	system("chcp 65001");
+	system("cls");
 
-	// //!测试用
-	// int a;
-	// scanf("%d",&a);
-	// int b = ((int)(3.4 + (a - 3) % 12 * 2.6)) % 7;
-	// int c = ((int) (3.4 + (3 - a) % 12 * 2.6)) % 7;
-	// printf("b:%d\n", b);
-	// printf("c:%d\n", c);
+		//? 调试用
+		// printf("argc:%d\n", argc);
+		// printf("argv0:%s\n", argv[0]);
+		// printf("argv1:%s\n", argv[1]);
+		// argv[1] = 1952;
 
+		// //? 测试func_LeapYear函数
+		// for (int i = 1950; i < 2050; i++)
+		// {
+		// 	if(func_LeapYear(i))
+		// 	// if (func_LeapYear(atoi(argv[1])))
+		// 		printf("%d ",i);
+		// 	// else
+		// 	// 	printf("flase");
+		// }
 
-	int year = 0;
+		// //!测试用
+		// int a;
+		// scanf("%d",&a);
+		// int b = ((int)(3.4 + (a - 3) % 12 * 2.6)) % 7;
+		// int c = ((int) (3.4 + (3 - a) % 12 * 2.6)) % 7;
+		// printf("b:%d\n", b);
+		// printf("c:%d\n", c);
+
+		int year = 0;
 	if (argc != 2)
 	{
 		char str[4];
 		printf("可接收程序参数(保留4位)main.exe <年份>\n例如main.exe 2022\n");
-		printf("\nmethod of application\tmain.exe {year}\nsuh as\tmain.exe 2022\n");
+		printf("method of application\tmain.exe {year}\nsuh as\tmain.exe 2022\n");
 		printf("输入一个值(input):");
 		scanf("%s", str);
 		str[4] = '\0'; // 防止溢出异常
@@ -71,6 +76,6 @@ int main(int argc, char **argv)
 
 	printf("%d年的1月1日是星期%d\n", year, (year_1_1 == 0 ? 7 : year_1_1));
 	printf("%d/1/1 = %d\n", year, (year_1_1 == 0 ? 7 : year_1_1));
-
+	system("pause");
 	return (0);
 }
